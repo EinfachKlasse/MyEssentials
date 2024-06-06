c6797c62e27c180552d5641e9943c409998413ba
kilianevers <76225977+kilianevers@users.noreply.github.com>
2024-06-06 19:27:28 +0200

    Initial commit

---
+++ src/main/java/de/couldbeworse/myessentials/MyEssentials.java
@@ -0,0 +1,17 @@
+package de.couldbeworse.myessentials;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MyEssentials extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
